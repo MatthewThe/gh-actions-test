7c6f468864a872bb302188bc47290b508dec1a47
MatthewThe <matthew.the@scilifelab.se>
2020-04-27 13:09:14 +0200

    Testing percolator emulated script with xcopy

--- test.bat
+++ test.bat
@@ -1,15 +1,16 @@
 type nul > file1.txt
 type nul > file2.txt
+type nul > file3.txt
 
 dir
 
-copy file1.txt ..\
+xcopy file1.txt ..\
 set /A exit_code=%ERRORLEVEL%
 
-copy file2.txt ..\
+xcopy file2.txt ..\
 set /A exit_code=exit_code+%ERRORLEVEL%
 
-copy file3.txt ..\
+xcopy file3.txt ..\
 set /A exit_code=exit_code+%ERRORLEVEL%
 
 echo Hello %exit_code%
