a3553d48062639b6872d771e9ff02f616d6c74f2
MatthewThe <matthew.the@scilifelab.se>
2020-04-27 13:05:04 +0200

    Testing percolator emulated script with backslashes

--- test.bat
+++ test.bat
@@ -3,17 +3,17 @@ type nul > file2.txt
 
 dir
 
-copy file1.txt ../
+copy file1.txt ..\
 set /A exit_code=%ERRORLEVEL%
 
-copy file2.txt ../
+copy file2.txt ..\
 set /A exit_code=exit_code||%ERRORLEVEL%
 
-copy file3.txt ../
+copy file3.txt ..\
 set /A exit_code=exit_code||%ERRORLEVEL%
 
 echo Hello
 
-cd ../
+cd ..\
 
 EXIT /B %exit_code%
