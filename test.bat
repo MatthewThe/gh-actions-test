0fc778ee89a96764b6557ad397aff3e726a4fbd9
MatthewThe <matthew.the@scilifelab.se>
2020-04-27 12:59:19 +0200

    Testing percolator emulated script with OR

--- test.bat
+++ test.bat
@@ -7,10 +7,10 @@ xcopy file1.txt ../
 set /A exit_code=%ERRORLEVEL%
 
 xcopy file2.txt ../
-set /A exit_code=exit_code+%ERRORLEVEL%
+set /A exit_code=exit_code||%ERRORLEVEL%
 
 xcopy file3.txt ../
-set /A exit_code=exit_code+%ERRORLEVEL%
+set /A exit_code=exit_code||%ERRORLEVEL%
 
 echo Hello
 
