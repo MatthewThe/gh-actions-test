3d932ca3ce79279b8f387a10c6d0bc0f43876dcc
MatthewThe <matthew.the@scilifelab.se>
2020-04-27 12:49:48 +0200

    Testing percolator emulated script

--- test.bat
+++ test.bat
@@ -4,7 +4,14 @@ type nul > file2.txt
 dir
 
 copy file1.txt file1_copy.txt
-copy file2.txt file2_copy.txt
-copy file3.txt file3_copy.txt
+set /A exit_code=%ERRORLEVEL%
 
-EXIT /B %ERRORLEVEL%
+copy file2.txt file2_copy.txt
+set /A exit_code=exit_code+%ERRORLEVEL%
+
+copy file3.txt file3_copy.txt
+set /A exit_code=exit_code+%ERRORLEVEL%
+
+echo Hello
+
+EXIT /B %exit_code%
