806535dd593d970548ee943e3ec15afb249e7259
MatthewThe <matthew.the@scilifelab.se>
2020-04-27 13:06:16 +0200

    Testing percolator emulated script with plusses

--- test.bat
+++ test.bat
@@ -7,12 +7,12 @@ copy file1.txt ..\
 set /A exit_code=%ERRORLEVEL%
 
 copy file2.txt ..\
-set /A exit_code=exit_code||%ERRORLEVEL%
+set /A exit_code=exit_code+%ERRORLEVEL%
 
 copy file3.txt ..\
-set /A exit_code=exit_code||%ERRORLEVEL%
+set /A exit_code=exit_code+%ERRORLEVEL%
 
-echo Hello
+echo Hello %exit_code%
 
 cd ..\
 
