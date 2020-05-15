5d876fafacb01322945c4f41c79330d79350805b
Matthew The <matthew.the@tum.de>
2020-05-15 11:36:04 +0200

    Testing where for errorlevel instead of xcopy

--- test.bat
+++ test.bat
@@ -5,9 +5,14 @@ type nul > file3.txt
 dir
 
 xcopy fl*e1.txt ..\
+where /Q fl*e1.txt
 set /A exit_code=%ERRORLEVEL%
 
 echo %exit_code%
+
+where /Q f*e1.txt
+echo %ERRORLEVEL%
+
 
 xcopy file2.txt ..\
 set /A exit_code=exit_code+%ERRORLEVEL%
