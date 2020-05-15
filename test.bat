d6d5ce9134261e9a9fe17d87a517c180ae02c59a
Matthew The <matthew.the@tum.de>
2020-05-15 11:26:45 +0200

    Testing wild card xcopy

--- test.bat
+++ test.bat
@@ -4,11 +4,15 @@ type nul > file3.txt
 
 dir
 
-xcopy file1.txt ..\
+xcopy f*e1.txt ..\
 set /A exit_code=%ERRORLEVEL%
+
+echo %exit_code%
 
 xcopy file2.txt ..\
 set /A exit_code=exit_code+%ERRORLEVEL%
+
+echo %exit_code%
 
 xcopy file3.txt ..\
 set /A exit_code=exit_code+%ERRORLEVEL%
