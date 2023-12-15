3efdb344de498fcf8b8faa8eb78b2068e23d2a27
Matthew The <matthew.the@tum.de>
2023-12-15 20:13:50 +0100

    add print statements for debugging

--- import_lxml.py
+++ import_lxml.py
@@ -1,2 +1,5 @@
 import PyInstaller.utils.hooks
+
+print("Trying to collect submodules for lxml.isoschematron")
 PyInstaller.utils.hooks._collect_submodules('lxml.isoschematron', 'warn once')
+print("Success??")
